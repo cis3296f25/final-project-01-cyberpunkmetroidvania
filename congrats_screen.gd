c35335a5059cec68622b70f7cae989d0c8d7b15b
jmcelhaugh <tup59389@temple.edu>
2025-12-03 18:49:45 -0500

    Many miscellaneous changes for polish!
    Commits these changes, longer list of changes in PR

---
+++ congrats_screen.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SoundController.stop_boss_music()
+	SoundController.play_congrats()
+	SoundController.play_ending_music()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().quit()
